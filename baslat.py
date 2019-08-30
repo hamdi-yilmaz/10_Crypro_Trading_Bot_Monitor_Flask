ef6bed56ec2fc604d0211523ab7083e225003783
Hamdi Yılmaz <hamdiyilmaz85@gmail.com>
2019-08-31 00:24:16 +0300

    Aktif işlemlere fiyat bilgisi eklmeke için yeni bir dataframe oluşturdum. bir sonraki aşamada dataframe i numpy yaparak devam edeceğim.

--- baslat.py
+++ baslat.py
@@ -2,11 +2,15 @@ from flask import Flask,render_template,request,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd 
 import numpy as np 
+from bittrex.bittrex import Bittrex
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/hh/Desktop/PycharmProjects 01092018/08_gun_bot/Bittrex/emir_takip.db'
 db = SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
+
+
+
 
 @app.route("/")
 def index():
@@ -53,9 +57,24 @@ def aktifEmirler():
     aktif_emirler =[]
     for emir in emirler:
         if emir.STATUS in ("Alim_emri_verildi" ,"ikincil_alim_emri_verildi", "Satim_emri_verildi", "ikincil_satim_emri_verildi"): 
-            aktif_emirler.append(emir)
+            aktif_emirler.append(emir.__dict__)                                 # onemli. sqalchemyden verileri dict olarak aldi
+            
+    
+    # marketlerden fiyat bilgisi ceker
+    panda_aktif_emirler = pd.DataFrame(aktif_emirler)
+    g = panda_aktif_emirler.groupby(['BORSA', 'MARKET'])
 
-    return render_template("aktif.html",orders = aktif_emirler)
+    ####!!!!! grup anahtarlarini numpy list yaparak devam edecegim. datafrema olmasi mantiksiz
+    gruplanmis = pd.DataFrame(g.groups.keys())                                    # grup anahtarlarini dataframe yapti
+      
+    def fiyat_bilgisi_al(Borsa,Market):
+        if Borsa == Bittrex:
+            return my_bittrex.get_ticker(Market)['result']['Last']
+   # gruplanmis['fiyat'] = gruplanmis.apply (lambda row: fiyat_bilgisi_al(row.0,row.1), axis=1)                                                      # yeni sutun ekledi  
+    print gruplanmis                   
+    deneme = panda_aktif_emirler.to_dict('records') 
+    
+    return render_template("aktif.html",orders = aktif_emirler )
 
 @app.route("/tum_emirler")
 def tumEmirler():
@@ -90,21 +109,21 @@ def tumEmirler():
             emir_durumu1 = dict(TIME=emir.TIME_ikinci, STATUS="Satim Emri Aktif", PRICE=emir.SELL_PRICE, AMOUNT=emir.SELL_AMOUNT, ORDER_NUMBER=emir.SELL_ORDER_NUMBER,	MARKET=emir.MARKET, KAZANC_ORANI=emir.KAZANC_ORANI)
             tum_emirler.append(emir_durumu1)
     
-    
+    # print tum_emirler
     panda_emirler = pd.DataFrame(tum_emirler)                               # panda dataframe e donustu
+    # print panda_emirler
     panda_emirler = panda_emirler.sort_values(by=['TIME'])                  # tarihe gore siraladi
     panda_emirler['NUMARA'] = np.arange(len(panda_emirler))                 # numara sutunu ekledi(pandas). dataframe uzunlugunda sirali sayi ekledi(numpy)
     tum_emirler = panda_emirler.to_dict('records')                          # dataframe i tekrar list of dict yapti. 'index' parametresi index numarasi ekliyor
 
     return render_template("tum_emirler.html",orders = tum_emirler)
 
+
+    
+
 @app.route("/durum")
 def finansalDurum():
-    bittrex_hesap = hesap.query.filter_by(BORSA="Bittrex").first()
-
-    from bittrex.bittrex import Bittrex
-    my_bittrex = Bittrex(bittrex_hesap.KEY,
-                     bittrex_hesap.SECRET)
+    
     available_btc = my_bittrex.get_balance('BTC')['result']['Available']
     bittrex_cuzdan = my_bittrex.get_balances()
 
@@ -174,5 +193,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 """
 
+bittrex_hesap = hesap.query.filter_by(BORSA="Bittrex").first()
+my_bittrex = Bittrex(bittrex_hesap.KEY,
+                     bittrex_hesap.SECRET)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
